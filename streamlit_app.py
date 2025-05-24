3e240bf5b331cd001494819e90c3e082e848388f
Nadha Nasari <nadhanasari@Mac.lan>
2025-05-24 20:38:12 +1000

    error

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="SkillQ", page_icon="🧠", layout="centered")
+st.title("Hello SkillQ 👋")
+st.markdown("This is your MVP base. Let’s build something amazing.")
+
+#fixed error
